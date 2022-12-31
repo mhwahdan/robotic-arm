90f6524536708b882410e0b6b3c9c1cf83a11e14
mhwahdan <moustafawahdan55@gmail.com>
2022-12-31 19:41:28 +0000

    Added Inverse Kinematics

--- RoboticArm.py
+++ RoboticArm.py
@@ -1,6 +1,7 @@
 import time 
 import RPi.GPIO as GPIO 
-
+import tinyik
+import numpy as np
 
 def initializeGPIO():
     GPIO.setmode(GPIO.BOARD)
@@ -18,30 +19,45 @@ class RoboticArm():
         self.__shoulderMotor = GPIO.PWM(shoulderMotorPin, PULSE_FREQ)
         self.__elbowMotor = GPIO.PWM(elbowMotorPin, PULSE_FREQ)
         self.__wristMotor = GPIO.PWM(wristMotorPin, PULSE_FREQ)
-        #Center all motors on 
-        self.__baseMotor.start(0)
-        self.__baseMotor.ChangeDutyCycle(7)
-        time.sleep(0.5)
-        #self.__baseMotor.stop()
-        self.__shoulderMotor.start(0)
-        self.__shoulderMotor.ChangeDutyCycle(7)
-        time.sleep(0.5)
-        #self.__shoulderMotor.stop()
-        self.__elbowMotor.start(0)
-        self.__elbowMotor.ChangeDutyCycle(7)
-        time.sleep(0.5)
-       #self.__elbowMotor.stop()
-        self.__wristMotor.start(0)        
-        self.__wristMotor.ChangeDutyCycle(7)
-        time.sleep(0.5)
-        #self.__wristMotor.stop()
+        self.Actuator = tinyik.Actuator(['x', [9., 0, 0], 'z', [10., 0, 0], 'z', [12., 0, 0], 'z', [3., 0, 0]])
+        self.Actuator.angles = np.deg2rad([90, 45, 45, 90])
+        self.position = self.Actuator.ee
+        self.reset()
         return
-        
+    
+    def reset(self):
+        self.setBaseMotorAngle(90)
+        self.setElbowMotorAngle(45)
+        self.setShoulderMotorAngle(45)
+        self.setElbowMotorAngle(45)
+        self.setWristMotorAngle(90)
+    
+    def inverseKinematics(self, x = 0, y = 0):
+          _x, _y, _z = self.Actuator.ee
+          _x += (y * -1)
+          _y += x
+          self.Actuator.ee = _x, _y, _z
+          angles =  np.round(np.rad2deg(self.Actuator.angles))
+          print(angles)
+          self.setBaseMotorAngle(angles[0])
+          self.setShoulderMotorAngle(angles[1])
+          self.setElbowMotorAngle(angles[2])
+          self.setWristMotorAngle(angles[3])
+          return angles
+      
+    def forwardKinmatics(self, baseMotorAngle, shoulderMotorAngle, elbowMotorAngle, wristMotorAngle):
+        self.Actuator.angles = np.deg2rad([baseMotorAngle, shoulderMotorAngle, elbowMotorAngle])
+        self.setBaseMotorAngle(self.Actuator.angles[0])
+        self.setShoulderMotorAngle(self.Actuator.angles[1])
+        self.setElbowMotorAngle(self.Actuator.angles[2])
+        self.setElbowMotorAngle(self.Actuator.angles[3])
+        return
+    
     def setBaseMotorAngle(self, angle):
         if(angle > 180 or angle < 0):
             return False
         
-        numberOfDegrees = int(angle / 18) + 2
+        numberOfDegrees = float(angle) / 18 + 2
         self.__baseMotor.start(0)
         self.__baseMotor.ChangeDutyCycle(numberOfDegrees)
         time.sleep(1)
@@ -52,7 +68,7 @@ class RoboticArm():
     def setShoulderMotorAngle(self, angle):
         if(angle > 90 or angle < 0):
             return False
-        numberOfDegrees = int(angle / 20) + 3
+        numberOfDegrees = float(angle) / 20 + 3
         self.__shoulderMotor.start(0)
         self.__shoulderMotor.ChangeDutyCycle(numberOfDegrees)
         time.sleep(1)
@@ -62,7 +78,7 @@ class RoboticArm():
     def setElbowMotorAngle(self, angle):
         if(angle > 90 or angle < 0):
             return False
-        numberOfDegrees = int(angle / 18) + 2
+        numberOfDegrees = float(angle) / 18 + 2
         self.__elbowMotor.start(0)
         self.__elbowMotor.ChangeDutyCycle(numberOfDegrees)
         time.sleep(1)
@@ -72,7 +88,7 @@ class RoboticArm():
     def setWristMotorAngle(self, angle):
         if(angle > 90 or angle < 0):
             return False
-        numberOfDegrees = int(angle / 18) + 2
+        numberOfDegrees = float(angle) / 18 + 2
         self.__wristMotor.start(0)
         self.__wristMotor.ChangeDutyCycle(numberOfDegrees)
         time.sleep(1)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import RoboticArm
 import time 
-
+import numpy as np
 '''
 Base Motor Pin => 11
 
@@ -16,18 +16,24 @@ RoboticArm.initializeGPIO()
 Robot  = RoboticArm.RoboticArm(baseMotorPin = 11, 
                                shoulderMotorPin = 13,
                                elbowMotorPin = 15,
-                               wristMotorPin = 16)                                                          
-while True:
-    Robot.setBaseMotorAngle(0)
-    Robot.setShoulderMotorAngle(0)
-    Robot.setElbowMotorAngle(0)
-    Robot.setWristMotorAngle(0)
-    time.sleep(1)
-    Robot.setBaseMotorAngle(90)
-    Robot.setShoulderMotorAngle(90)
-    Robot.setElbowMotorAngle(90)
-    Robot.setWristMotorAngle(90)
-    time.sleep(1)
+                               wristMotorPin = 16)
+Robot.reset()
+Robot.inverseKinematics(x = 5 , y = 5)
 
+# while True:
+#     try:
+#         Robot.setBaseMotorAngle(0)
+#         Robot.setShoulderMotorAngle(0)
+#         Robot.setElbowMotorAngle(0)
+#         Robot.setWristMotorAngle(0)
+#         time.sleep(1)
+#         Robot.setBaseMotorAngle(90)
+#         Robot.setShoulderMotorAngle(90)
+#         Robot.setElbowMotorAngle(90)
+#         Robot.setWristMotorAngle(90)
+#         time.sleep(1)
+#     except KeyboardInterrupt:
+#         RoboticArm.terminateGPIO()
+#         exit()
 
 RoboticArm.terminateGPIO()
