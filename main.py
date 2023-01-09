483d297ad4e3155cc006a28aff735d8952160e6b
mhwahdan <61120913+mhwahdan@users.noreply.github.com>
2023-01-09 12:44:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import RoboticArm
+from math import pi
 
 '''
 Base Motor Pin => 11
@@ -15,7 +16,30 @@ RoboticArm.initializeGPIO()
 Robot  = RoboticArm.RoboticArm(baseMotorPin = 11, 
                                shoulderMotorPin = 13,
                                elbowMotorPin = 15,
-                               wristMotorPin = 16)
+                               wristMotorPin=16,
+                               DH_PARAMS=
+                               [[11.5, 0., pi / 2, 0],
+                                [0., 10., 0, 0 ],
+                                [0., 12.7, 0, 0],
+                                [0, 2.8, 0, 0]],
+                               maximumIterations=750)
 
+Robot.inverseKinematics(27.5, 0, 9)
+
+# while True:
+#     try:
+#         Robot.setBaseMotorAngle(0)
+#         Robot.setShoulderMotorAngle(0)
+#         Robot.setElbowMotorAngle(0)
+#         Robot.setWristMotorAngle(0)
+#         time.sleep(1)
+#         Robot.setBaseMotorAngle(90)
+#         Robot.setShoulderMotorAngle(90)
+#         Robot.setElbowMotorAngle(90)
+#         Robot.setWristMotorAngle(90)
+#         time.sleep(1)
+#     except KeyboardInterrupt:
+#         RoboticArm.terminateGPIO()
+#         exit()
 
 RoboticArm.terminateGPIO()
